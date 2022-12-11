62ae746fa85f65a8f04294608063b11338bd7cff
Najah Izquierdo <najahizquierdo@gmail.com>
2022-12-11 01:32:11 -0800

    Add files via upload
    - The Logistic Regression is only working using lgm 
    - KNN takes FOREVER on my laptop, but still works! just takes ages to load. 
    - Dtree was giving me .79 accuracy but then it started returning 1? not sure what that was about lol 
    - Included the Data preprocessing file too, I added a few things. 
    - Feel free to check and let me know what I can change. This is one of the first times I've modeled in R so hopefully it isn't awful lmao

---
+++ Model.R
@@ -0,0 +1,115 @@
+# import dataset
+source("health_care_analytics.R")
+# Loading package
+library(dplyr)
+library(ROCR)
+library(caTools)
+library(caret)
+library(pROC)
+library(e1071)
+library(class)
+library(tidyverse)
+install.packages("gmodels")
+
+
+
+# Splitting dataset
+train_data <- createDataPartition(y = data_red3$Stay,p = 0.75,list = FALSE)
+train_dataset <- data_red3[train_data,]
+test_dataset <- data_red3[-train_data,]
+
+#check distribution
+prop.table(table(train_dataset$Stay)) * 100
+prop.table(table(test_dataset$Stay)) * 100
+prop.table(table(data_red3$Stay)) * 100
+
+
+#Preprocessing for KNN and Logistic Reg
+training_dataX <- train_dataset[,names(train_dataset) != "Stay"]
+dummy <- dummyVars( ~ ., data=training_dataX)
+dummy_train <- data.frame(predict(dummy, newdata = train_dataset))
+dummy_test <- data.frame(predict(dummy, newdata = test_dataset))
+dummy_test <- na.omit(dummy_test)
+dummy_train <- na.omit(dummy_train)
+train_dataset <- na.omit(train_dataset)
+
+
+set.seed(400)
+
+# Logistic Regression
+trControl <- trainControl(method = 'repeatedcv', number = 5,search = 'random')
+train_dataset$StayNum <- as.factor(as.numeric(train_dataset$Stay))
+logistic_model <- glm(train_dataset$StayNum ~ ., 
+                      data = dummy_train, 
+                      family = "binomial")
+summary(logistic_model)
+confusionMatrix(
+  factor(logistic_model), 
+  factor(train_dataset$StayNum),
+  positive = NULL,
+  prevalence = NULL,
+  mode = "sens_spec"
+)
+#plot(logistic_model, col="steelblue")
+
+
+#Naive Bayes 
+#takes long as well
+naive_bayes <- train(Stay ~ ., 
+                      data = train_dataset,
+                      method = "naive_bayes",
+                      usepoisson = TRUE,
+                      na.action = na.pass)
+naive_bayes
+confusionMatrix(
+  naive_bayes,
+  positive = NULL,
+  prevalence = NULL,
+  mode = "sens_spec"
+)
+#.9878
+
+
+#KNN - this takes AGES but it works, I swear
+ctrl_knn <- trainControl(method = "cv",
+                                 summaryFunction = defaultSummary,
+                                 number = 10)
+set.seed(2)
+knn <- train(Stay ~ ., 
+                data = train_dataset,
+                method = "knn",
+                trControl=ctrl_knn
+                metric = "Accuracy")
+knn
+confusionMatrix(
+  knn,
+  positive = NULL,
+  prevalence = NULL,
+  mode = "sens_spec"
+)
+
+#
+
+#Decision Trees
+
+ctrl_dtree <- trainControl(method="repeatedcv",repeats = 3)
+dtree_model <- train(Stay ~., 
+                data = train_dataset, 
+                method = "rpart",
+                parms = list(split = "information"),
+                trControl=ctrl_dtree,
+                tuneLength = 10,
+                na.action = na.pass)
+print(dtree_model)
+confusionMatrix(
+  dtree_model,
+  positive = NULL,
+  prevalence = NULL,
+  mode = "sens_spec"
+)
+#.7923
+plot(dtree_model, col="steelblue")
+
+  
+  
+  
